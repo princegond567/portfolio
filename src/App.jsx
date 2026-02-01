9cfb22e5ad012c670231eca0a4831400a1841363
Prince Gond <princegond811510@gmail.com>
2026-02-01 19:27:38 +0530

    Added home page

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css'
+import Navbar from './components/Navbar'
+import Home from './sections/Home'
+import About from './sections/About'
+import Skills from './sections/Skills'
+import Project from './sections/Project'
+import Experience from './sections/Experience'
+import Testimonials from './sections/Testimonials'
+import Contact from './sections/Contact'
+import Footer from './sections/Footer'
+// import ParticlesBackground from './components/ParticlesBackground'
+import CustomCursor from './components/CustomCursor'
+
+function App() {
+
+  return (
+    <div className='relative gradient text-white'>
+      <CustomCursor/>
+      {/* <ParticlesBackground/> */}
+      <Navbar/>
+      <Home/>
+      <About/>
+      <Skills/>
+      <Project/>
+      <Experience/>
+      <Testimonials/>
+      <Contact/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/IntroAnimation.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const IntroAnimation = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default IntroAnimation
